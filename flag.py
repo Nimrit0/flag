d8b5c839ae0a8c8717563f51a9b7b27c412bd326
Nimrit0 <119907635+Nimrit0@users.noreply.github.com>
2025-03-11 18:00:25 +0545

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,10 +1,9 @@
 import base64
 
-# The flag is encoded in base64 to hide it
+
 encoded_flag = "bmM0e3M0Y2Nlc3NmMGxseV9kMHdubDA0ZGVkX0ZyMG1fZzF0aHVifQ=="
 
-# Decode the flag at runtime
 flag = base64.b64decode(encoded_flag).decode('utf-8')
 
-# Display the flag
+
 print(flag)
